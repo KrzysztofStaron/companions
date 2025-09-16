e0e218136a47124b2c155ad6aef0822d456d85fe
Krzysztof Staroń <kisiel3141@gmail.com>
2025-09-16 22:14:49 +0200

    pls work

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AudioPermissionProvider } from "./components/AudioPermissionManager";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AudioPermissionProvider>{children}</AudioPermissionProvider>
+        <Analytics />
       </body>
     </html>
   );
