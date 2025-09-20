70e2e163279a22f19faf450a7cd91678e739ada6
Krzysztof Staroń <kisiel3141@gmail.com>
2025-09-20 23:11:03 +0200

    send icon

--- app/page.tsx
+++ app/page.tsx
@@ -838,18 +838,18 @@ export default function Home() {
               title="Send message"
             >
               <svg
-                width="20"
-                height="20"
+                width="18"
+                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                strokeWidth="2"
+                strokeWidth="2.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="text-white"
               >
-                <path d="m22 2-7 20-4-16-4 16L2 22l20-7Z" />
-                <path d="M10.5 15.5 9 9l6 6" />
+                <path d="M22 2L11 13" />
+                <path d="m22 2-7 20-4-9-9-4 20-7z" />
               </svg>
             </button>
           </div>
